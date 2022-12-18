b20bede369575ae2dee1884e12f047524b52c2c6
karthi <karthirajendran12003@gmail.com>
2022-12-18 20:14:26 +0530

    chatbot

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors=require("cors")
 const mongoose=require("mongoose")
 const dotenv = require("dotenv")
 const path=require('path')
-
+const SerpApi =require('google-search-results-nodejs');
+const search = new SerpApi.GoogleSearch("b87e2c676fbba149af95c0f8e39c7a28fdeb9cec46c5a3cd57b7a8d2de244a4e");
 const logRoutes =require("./routes/logs.js")
 const doctorRoutes = require('./routes/doctor.js')
 const hospitalRoutes = require('./routes/hospital.js')
@@ -29,7 +30,35 @@ app.use('/logs',logRoutes)
 app.get('/',(req,res)=>{
     res.send("DIGITAL CARE")
 })
+
+app.post('/chatbot',async(req,res)=>{
+    const {message} = req.body
+  const params = {
+    q: `${message}`,
+    location: "India",
+    google_domain: "google.com",
+    hl: "en",
+    gl: "us"
+  };
+  try{
+  const callback = function(data) {
+    res.status(200).json(data)
+  };
+  
+  // Show result as JSON
+  search.json(params, callback);
+  // console.log(ansData);
+  // res.status(200).json(ansData)
+  
+}catch(err){
+  res.status(500).json({message:err.message})
+}
+})
 const PORT = process.env.PORT || 8080;
 app.listen(PORT ,()=>{
     console.log(`Server is listening on ${PORT}`)
 })
+
+
+
+
